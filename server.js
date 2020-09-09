618811bbd467b7e0abedba0b7d495d77bba508a4
Amir-Benyamini <amir.benyamini.ppc@gmail.com>
2020-09-09 17:45:25 +0300

    client

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
-const app = express()
 const bodyParser = require('body-parser')
+const path = require('path')
+const app = express()
+
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'dist')))
 
 
 const port = 3500
